df457d266d1121e89bf7e39019b193657f885626
RC_Student_lab <RC_Student_lab@10.0.0.162>
2024-09-09 07:28:59 +0000

    First commit

---
+++ src/main/java/com/mycompany/firstprijecttest/FirstPrijectTest.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.firstprijecttest;
+
+/**
+ *
+ * @author RC_Student_lab
+ */
+public class FirstPrijectTest {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
