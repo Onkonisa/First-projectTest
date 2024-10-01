9d55ee6d6a48a991bd0af1201291666e5cc6b45c
RC_Student_lab <RC_Student_lab@10.0.0.162>
2024-10-01 10:49:54 +0000

    Created a program for two numbers

---
+++ src/main/java/com/mycompany/firstprojecttest/FirstPrijectTest.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.firstprojecttest;
+
+/**
+ *
+ * @author RC_Student_lab
+ */
+public class FirstPrijectTest {
+
+    public static void main(String[] args){
+        
+        public class Logintest        
+      
+
+    private static class Login {
+
+        
+        public static void testLoginSuccessful() {
+         assertTrue (login.login("Mdu_" , "Quintonn@12")"Login must be successful with correct Username and Password" 
+        }
+        }
+        }
+    }
+    
+      
+
+    
+
---
+++ src/test/java/com/mycompany/firstprojecttest/FirstPrijectTestTest.java
@@ -0,0 +1,44 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit5TestClass.java to edit this template
+ */
+package com.mycompany.firstprojecttest;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ *
+ * @author RC_Student_lab
+ */
+public class FirstPrijectTestTest {
+    
+    public FirstPrijectTestTest() {
+    }
+
+    @org.junit.jupiter.api.BeforeAll
+    public static void setUpClass() throws Exception {
+    }
+
+    @org.junit.jupiter.api.AfterAll
+    public static void tearDownClass() throws Exception {
+    }
+
+    @org.junit.jupiter.api.BeforeEach
+    public void setUp() throws Exception {
+    }
+
+    @org.junit.jupiter.api.AfterEach
+    public void tearDown() throws Exception {
+    }
+     }
+
+    /**
+     * Test of main method, of class FirstPrijectTest.
+     */
+   
+
